6e5f97e930935628144c1dabf04e9da36ab418ce
Shijin <ss_shijin@yahoo.co.in>
2021-08-27 10:44:46 +0000

    server.js commit

--- server.js
+++ server.js
@@ -39,9 +39,17 @@ app.get('/version', (req, res) => {
 
 // mock events endpoint. this would be replaced by a call to a datastore
 // if you went on to develop this as a real application.
+/*
 app.get('/events', (req, res) => {
     res.json(mockEvents);
 });
+*/
+app.get('/events', (req, res) => {
+    getEvents(req, res);
+});
+
+
+
 
 // Adds an event - in a real solution, this would insert into a cloud datastore.
 // Currently this simply adds an event to the mock array in memory
@@ -73,3 +81,47 @@ const server = app.listen(PORT, () => {
 });
 
 module.exports = app;
+
+// bring in firestore
+const Firestore = require("@google-cloud/firestore");
+
+// initialize Firestore and set project id from env var
+const firestore = new Firestore(
+    {
+        projectId: process.env.GOOGLE_CLOUD_PROJECT
+    }
+);
+
+app.post('/event', (req, res) => {
+    // create a new object from the json data and add an id
+    const ev = { 
+        title: req.body.title, 
+        description: req.body.description,
+        id : mockEvents.events.length + 1
+     }
+// this will create the Events collection if it does not exist
+    firestore.collection("Events").add(ev).then(ret => {
+        getEvents(req, res);
+    });
+
+});
+
+function getEvents(req, res) {
+    firestore.collection("Events").get()
+        .then((snapshot) => {
+            if (!snapshot.empty) {
+                const ret = { events: []};
+                snapshot.docs.forEach(element => {
+                    ret.events.push(element.data());
+                }, this);
+                console.log(ret);
+                res.json(ret);
+            } else {
+                 res.json(mockEvents);
+            }
+        })
+        .catch((err) => {
+            console.error('Error getting events', err);
+            res.json(mockEvents);
+        });
+};
